480a770bca9fbe1b1ba41b2b616c6f1ab849cc4a
Lorenzo Tumini <lnz.tumini@outlook.it>
2026-07-03 19:08:11 +0200

    Initial commit

--- vim/vim.ps1
+++
@@ -1,9 +0,0 @@
-$Root = $PSScriptRoot
-$Nvim = Join-Path $HOME ".local\share\chezmoi\AppData\Local\nvim"
-
-$File = Join-Path $Root ".\ftdetect"
-Copy-Item -Path $File -Destination $Nvim -Force -Recurse
-$Syntax = Join-Path $Root ".\syntax"
-Copy-Item -Path $Syntax -Destination $Nvim -Force -Recurse
-
-chezmoi -v apply
